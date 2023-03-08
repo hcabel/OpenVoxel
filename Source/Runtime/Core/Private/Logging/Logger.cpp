d54d0b04bce110c9b31017c99bca6614310749c8
hcabel <hcabel@student.42.fr>
2023-03-08 13:53:09 +1000

    Remove extra \n when printing Logs

--- Source/Runtime/Core/Private/Logging/Logger.cpp
+++ Source/Runtime/Core/Private/Logging/Logger.cpp
@@ -2,7 +2,7 @@
 
 void Logger::Log(Verbosity::Type verbosity, LogCategory& category, std::string message)
 {
-	std::string logMessage = std::format("[{}]: {}: {}\n", category.GetName(), Verbosity::ToString(verbosity), message);
+	std::string logMessage = std::format("[{}]: {}: {}", category.GetName(), Verbosity::ToString(verbosity), message);
 	// TODO: Log onto the appropriate place depending on the verbosity like describe in the Verbosity enum
 	LogOntoConsole(logMessage);
 }
