e30e921dc3b4d1c1be7bafceb1c91a532c199aa0
hcabel <hcabel@student.42.fr>
2023-03-14 10:20:01 +1000

    Create global CoreLog logging category

--- Source/Runtime/Core/Public/CoreModule.h
+++ Source/Runtime/Core/Public/CoreModule.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include "Logging/LoggingMacros.h"
+
 #if OV_BUILD_DLL
 # define CORE_API __declspec(dllexport)
 #else
 # define CORE_API __declspec(dllimport)
 #endif
+
+DEFINE_LOG_CATEGORY(CoreLog);
