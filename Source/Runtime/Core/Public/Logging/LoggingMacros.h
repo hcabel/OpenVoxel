3f518265a43ff772d6ee1275ace9b94a79734370
hcabel <hcabel@student.42.fr>
2023-04-07 17:50:34 +1000

    Fix Logging macros
    OV_LOG_IF behavior does not change because it still has to crash the application if `condition && verboity == fatal`
    OV_LOG_ARRAY should not be completly remove but instead replace by the FATAL_CHECK

--- Source/Runtime/Core/Public/Logging/LoggingMacros.h
+++ Source/Runtime/Core/Public/Logging/LoggingMacros.h
@@ -4,13 +4,15 @@
 #include "Logging/LoggingType.h"
 #include "Logging/Logger.h"
 
+// create a more explicit logging macro if logging is enabled
+#define WITH_LOGGING !NO_LOGGING
+
 #if NO_LOGGING
 
 # define OV_LOG(LogVerbosity, Category, Format, ...) \
 	if (Verbosity::LogVerbosity == Verbosity::Fatal) \
 		assert(false);
-# define OV_LOG_ARRAY(LogVerbosity, Category, Array, Format, ...) EMPTY_MACRO
-# define OV_LOG_IF(Condition, LogVerbosity, Category, Format, ...) EMPTY_MACRO
+# define OV_LOG_ARRAY(LogVerbosity, Category, Array, Format, ...) OV_LOG(LogVerbosity, Category, Format, __VA_ARGS__)
 
 # define DECLARE_LOG_CATEGORY(CategoryName) EMPTY_MACRO
 # define DEFINE_LOG_CATEGORY(CategoryName) EMPTY_MACRO
@@ -28,14 +30,14 @@
 	FOR_EACH_LOOP(Entry, Array) \
 		OV_LOG(LogVerbosity, Category, Format, Entry, __VA_ARGS__); \
 
-# define OV_LOG_IF(Condition, LogVerbosity, Category, Format, ...) \
-	if (Condition) \
-		OV_LOG(LogVerbosity, Category, Format, __VA_ARGS__)
-
 # define DECLARE_LOG_CATEGORY(CategoryName) extern class LogCategory CategoryName;
 # define DEFINE_LOG_CATEGORY(CategoryName) LogCategory CategoryName(#CategoryName);
 
 #endif // !NO_LOGGING || WITH_LOGGING
 
-// create a more explicit logging macro if logging is enabled
-#define WITH_LOGGING !NO_LOGGING
+/* MACRO THAT DOES NOT CHANGE WHEN LOGGING IS DISABLED */
+
+// Log if condition is true
+# define OV_LOG_IF(Condition, LogVerbosity, Category, Format, ...) \
+	if (Condition) \
+		OV_LOG(LogVerbosity, Category, Format, __VA_ARGS__)
