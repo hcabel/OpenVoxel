6636d78e0b804318ca4e933cf251672bbd1be72c
hcabel <hcabel@student.42.fr>
2023-06-18 00:07:23 +1000

    Create build command for the Premake project
    This build command will regenerate the solution.

--- vendor/Premake5/premake5.lua
+++ vendor/Premake5/premake5.lua
@@ -3,8 +3,16 @@ project "Premake"
 	
 	location (projectFileLocation)
 
+	targetdir (buildOutput .. "/%{prj.name}")
+	objdir (intermediateOutput .. "/%{prj.name}")
+
 	files
 	{
 		"**.lua",
 		"%{wks.location}/**premake5.lua"
+	}
+
+	postbuildcommands
+	{
+		"%{wks.location}/GenerateSolution.bat"
 	}
