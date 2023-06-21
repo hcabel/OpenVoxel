8bd6960f51ad58fa0e5f18e615cc3689b6f30810
hcabel <hcabel@student.42.fr>
2023-06-22 00:19:03 +1000

    Add premake file to build the core module has a DLL

---
+++ Source/Runtime/Core/premake5.lua
@@ -0,0 +1,47 @@
+﻿
+include "../../../vendor/Premake5/GlobalVariable.lua"
+
+project "Core"
+	kind "SharedLib"
+	language "C++"
+	cppdialect "C++20"
+
+	targetdir (buildOutput .. "/OpenVoxel")
+	objdir (intermediateOutput .. "/OpenVoxel")
+
+	location (projectFileLocation)
+
+	files
+	{
+		"**.h",
+		"**.cpp"
+	}
+
+	includedirs
+	{
+		"Private",
+		"Public",
+		"../../ThirdParty/GLFW/include",
+		"../../ThirdParty/glm",
+	}
+
+	links
+	{
+		"GLFW",
+	}
+
+	staticruntime "off"
+	systemversion "latest"
+
+	filter "configurations:*Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:*Release"
+		runtime "Release"
+		optimize "on"
+		symbols "off"
+	filter {}
+
+	defines "PLATFORM_WINDOWS"
+	defines "OV_BUILD_DLL"
