1f15957597593fe0f8d17e830021ad5608687317
hcabel <hcabel@student.42.fr>
2023-07-10 13:23:24 +1000

    When a module rebuild the dll is not moved if already exist
    Fixes #47
    I fix it by disabling the fast up to date check, which will trigger the post command even if no change where made to the project

--- vendor/Premake5/Project.lua
+++ vendor/Premake5/Project.lua
@@ -1,11 +1,16 @@
 ﻿include "GlobalVariable.lua"
 include "Shared.lua"
 
+include "Extended/FastUpToDateCheck.lua" -- Allow the project to disable fast up to date check
+
 ProjectTargetOutput = BuildOutput .. "/%{prj.name}"
 ProjectObjectOutput = IntermediateOutput .. "/%{prj.name}"
 
 function UseProjectDefaultConfig()
 	UseDefines()
+
+	-- Disable Fast Up To Date check, to allow the project to update the dlls after building modules
+	FastUpToDateCheck(false)
 
 	language "C++"
 	cppdialect "C++20"
