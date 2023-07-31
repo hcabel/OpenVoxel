0c92ba71d97758bad4394aacb469be57ee3a4ee6
hcabel <hcabel@student.42.fr>
2023-07-31 14:53:47 +1000

    Add "saved" and "log" directory path getter in Path The "save" directory path is the path to the directory where all the generated file are. And the "log" directory path is the path to the folder that store all the generated log file (it's in th save folder)

--- Source/Runtime/Core/Private/Path.cpp
+++ Source/Runtime/Core/Private/Path.cpp
@@ -5,27 +5,40 @@
 
 Path::DataCache Path::s_Data;
 
+/**
+ * Create the content of a function that will get a path from the cache
+ * if the cache is empty, it will call the getter function in the PlatformFileSystem to get the path.
+ * @param VariableName the name of the variable that will be used to store the path in the cache
+ * @note The getter function must be named Make[VariableName]Path in the PlatformFileSystem class
+ */
+#define PATH_GETTER_CACHED(VariableName) \
+	/* Get the VariableName from in cache */ \
+	std::string& VariableName = s_Data.VariableName; \
+	if (VariableName.empty()) \
+	{ \
+		/* if cache is empty, get the value using the getter function */ \
+		VariableName = PlatformFileSystem::Make##VariableName##Path(); \
+	} \
+	return (VariableName); \
+
 std::string Path::GetEngineRootDirectoryPath()
 {
-	// Get the root directory from in cache
-	std::string& rootDirectory = s_Data.EngineRootDirectory;
-	if (rootDirectory.empty())
-	{
-		// if cache is empty, create the root directory path
-		rootDirectory = PlatformFileSystem::MakeEngineRootDirectoryPath();
-	}
-	return (rootDirectory);
+	PATH_GETTER_CACHED(EngineRootDirectory);
 }
 
 std::string Path::GetModuleDirectoryPath()
 {
-	// Get the module directory from in cache
-	std::string& moduleDirectory = s_Data.EngineRootDirectory;
-	if (moduleDirectory.empty())
-	{
-		// if cache is empty, create the module directory path
-		moduleDirectory = PlatformFileSystem::MakeModuleDirectoryPath();
-	}
-	return (moduleDirectory);
-
+	PATH_GETTER_CACHED(ModuleDirectory);
 }
+
+std::string Path::GetLogDirectoryPath()
+{
+	return (GetSavedDirectoryPath() + "logs\\");
+}
+
+std::string Path::GetSavedDirectoryPath()
+{
+	return (GetEngineRootDirectoryPath() + "saved\\");
+}
+
+#undef PATH_GETTER_CACHED
