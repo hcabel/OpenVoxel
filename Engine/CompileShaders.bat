28f7cd284bde4e5f8e8b06592295b1a7d4f7c12c
hcabel <hcabel@student.42.fr>
2023-08-08 10:32:37 +1000

    Update compile shader script to create a target folder I updated the compile shader script so it create the Shader target folder before compiling (but it need to be changed so it's compiled at build time)

--- Engine/CompileShaders.bat
+++ Engine/CompileShaders.bat
@@ -1,5 +1,15 @@
-call vendor\bin\glslc.exe --target-env=vulkan1.3 Source/Runtime/Renderer/private/Shaders/raytrace.rgen -o intermediate/Shaders/RayGen.spv
-call vendor\bin\glslc.exe --target-env=vulkan1.3 Source/Runtime/Renderer/private/Shaders/raytrace.rmiss -o intermediate/Shaders/Miss.spv
-call vendor\bin\glslc.exe --target-env=vulkan1.3 Source/Runtime/Renderer/private/Shaders/raytrace.rchit -o intermediate/Shaders/Hit.spv
-call vendor\bin\glslc.exe --target-env=vulkan1.3 Source/Runtime/Renderer/private/Shaders/raytrace.rint -o intermediate/Shaders/Intersection.spv
+@echo off
+
+set "SHADER_PATH=Source/Runtime/Renderer/private/Shaders"
+set "SHADER_TARGET_PATH=intermediate/Shaders"
+
+@echo on
+
+if not exist "%SHADER_TARGET_PATH%" mkdir "%SHADER_TARGET_PATH%"
+
+call vendor\bin\glslc.exe --target-env=vulkan1.3 %SHADER_PATH%/raytrace.rgen -o %SHADER_TARGET_PATH%/RayGen.spv
+call vendor\bin\glslc.exe --target-env=vulkan1.3 %SHADER_PATH%/raytrace.rmiss -o %SHADER_TARGET_PATH%/Miss.spv
+call vendor\bin\glslc.exe --target-env=vulkan1.3 %SHADER_PATH%/raytrace.rchit -o %SHADER_TARGET_PATH%/Hit.spv
+call vendor\bin\glslc.exe --target-env=vulkan1.3 %SHADER_PATH%/raytrace.rint -o %SHADER_TARGET_PATH%/Intersection.spv
+
 pause
