285381bffc33a1a1e07541c84db2be76cb71c0f6
hcabel <hcabel@student.42.fr>
2023-08-31 21:52:22 +1000

    Remove the UI class

--- Engine/Source/Editor/UI/Private/UIModule.cpp
+++ Engine/Source/Editor/UI/Private/UIModule.cpp
@@ -1,20 +1,15 @@
 #include "UIModule.h"
-#include "UIGlobals.h"
-#include "UI.h"
-#include "Renderer.h"
+
+#include <GLFW/glfw3.h>
 
 IMPLEMENT_MODULE(UIModule);
 
 void UIModule::StartupModule()
 {
-	// glfwInit need to be call in each module that use any glfw function
 	glfwInit();
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-
-	UI::Get().Init();
 }
 
 void UIModule::ShutdownModule()
 {
-	UI::Get().ShutDown();
+	glfwTerminate();
 }
