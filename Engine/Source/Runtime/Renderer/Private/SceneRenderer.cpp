58e76b006dcaa549e068f81e359d8d5f92c20c67
hcabel <hcabel@student.42.fr>
2023-09-20 16:36:27 +1000

    Upgrade Scene renderer to call the ray tracing GPU acceleration I also moved from a constructor to a static create function. But the main thing is that now it's using all the vulkan objects to draw stuff on an image that is passed in parameter of the RenderScene function.

--- Engine/Source/Runtime/Renderer/Private/SceneRenderer.cpp
+++ Engine/Source/Runtime/Renderer/Private/SceneRenderer.cpp
@@ -1,8 +1,27 @@
-#include "SceneRenderer.h"
+﻿#include "SceneRenderer.h"
 #include "VulkanContext.h"
+#include "Profiling/ProfilingMacros.h"
 
-SceneRenderer::SceneRenderer()
+#include <glm/glm.hpp>
+
+SceneRenderer SceneRenderer::Create(uint32_t frameCount)
 {
+	std::vector<vk::DescriptorPoolSize> poolSizes;
+	poolSizes.push_back(
+		vk::DescriptorPoolSize(
+			vk::DescriptorType::eAccelerationStructureKHR,
+			1
+		)
+	);
+	// Create the descriptor pool
+	vk::DescriptorPool descriptorPool = VulkanContext::GetDevice().createDescriptorPool(
+		vk::DescriptorPoolCreateInfo(
+			vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet,
+			frameCount,
+			poolSizes.size(), poolSizes.data()
+		)
+	);
+
 	// Describe what is in the descriptor set and which shader has access to it
 	std::array<vk::DescriptorSetLayoutBinding, 2> descriptorSetLayoutBindings;
 	descriptorSetLayoutBindings[0] = vk::DescriptorSetLayoutBinding( // binding 0: Ray tracing image output
@@ -23,23 +42,129 @@ SceneRenderer::SceneRenderer()
 		vk::DescriptorSetLayoutCreateFlags(),
 		static_cast<uint32_t>(descriptorSetLayoutBindings.size()), descriptorSetLayoutBindings.data()
 	);
-	m_DescriptorSetLayout = VulkanContext::GetDevice().createDescriptorSetLayout(descriptorSetLayoutCreateInfo);
+
+	std::vector<vk::DescriptorSetLayout> descriptorSetLayouts(frameCount);
+	for (uint32_t i = 0; i < frameCount; i++)
+		descriptorSetLayouts[i] = VulkanContext::GetDevice().createDescriptorSetLayout(descriptorSetLayoutCreateInfo);
 
 	// Create the descriptor set
 	vk::DescriptorSetAllocateInfo descriptorSetAllocateInfo(
-		VK_NULL_HANDLE,
-		1,
-		&m_DescriptorSetLayout
+		descriptorPool,
+		frameCount, descriptorSetLayouts.data(),
+		nullptr
 	);
-	m_DescriptorSet = VulkanContext::GetDevice().allocateDescriptorSets(descriptorSetAllocateInfo)[0];
+
+	std::vector<vk::DescriptorSet> descriptorSets
+		= VulkanContext::GetDevice().allocateDescriptorSets(descriptorSetAllocateInfo, VulkanContext::GetDispatcher());
+
+	VoxelAccelerationStructure accelerationStructure = VoxelAccelerationStructure::Create();
+	accelerationStructure.AddVoxel();
+	accelerationStructure.Rebuild();
 
 	// Create the pipeline
-	m_Pipeline = VulkanRayTracingPipeline(m_DescriptorSetLayout);
+	VulkanRaytracingPipeline raytracingPipeline = VulkanRaytracingPipeline::Create(descriptorSetLayouts[0]);
+
+	return (SceneRenderer(
+		std::move(raytracingPipeline),
+		std::move(accelerationStructure),
+		std::move(descriptorPool),
+		std::move(descriptorSetLayouts),
+		std::move(descriptorSets)
+	));
 }
 
 SceneRenderer::~SceneRenderer()
 {
-	m_Pipeline.~VulkanRayTracingPipeline();
+	if (m_DescriptorSets.size() > 0)
+		VulkanContext::GetDevice().freeDescriptorSets(m_DescriptorPool, m_DescriptorSets.size(), m_DescriptorSets.data());
 
-	VulkanContext::GetDevice().destroyDescriptorSetLayout(m_DescriptorSetLayout);
+	if (m_DescriptorPool)
+		VulkanContext::GetDevice().destroyDescriptorPool(m_DescriptorPool);
+
+	for (uint32_t i = 0; i < m_DescriptorSetLayouts.size(); ++i)
+		VulkanContext::GetDevice().destroyDescriptorSetLayout(m_DescriptorSetLayouts[i]);
+}
+
+void SceneRenderer::RenderScene(const SceneRenderer::RenderSceneInfo& inInfo)
+{
+	CREATE_PERFRAME_SCOPE_TIMER("Updating acceleration structure");
+
+	if (m_AccelerationStructure.IsRebuildNeeded() == true)
+		m_AccelerationStructure.Rebuild();
+
+	inInfo.commandBuffer.bindPipeline(
+		vk::PipelineBindPoint::eRayTracingKHR,
+		m_Pipeline
+	);
+
+	// update descriptor set
+	{
+		std::vector<vk::WriteDescriptorSet> descriptorWrites;
+
+		// Write the current image view on the descriptor set (layout location 1)
+		vk::DescriptorImageInfo imageInfo(
+			vk::Sampler(),
+			inInfo.outputImage,
+			vk::ImageLayout::eGeneral
+		);
+		descriptorWrites.push_back(
+			vk::WriteDescriptorSet(
+				m_DescriptorSets[inInfo.frameIndex],
+				0,
+				0,
+				1, vk::DescriptorType::eStorageImage,
+				&imageInfo,
+				nullptr,
+				nullptr
+			)
+		);
+
+		// Get TLAS and write it on the descriptor set (layout location 0)
+		vk::AccelerationStructureKHR accelerationStructure = m_AccelerationStructure.GetTLAS();
+		vk::WriteDescriptorSetAccelerationStructureKHR accelerationStructureInfo(
+			1, &accelerationStructure
+		);
+		descriptorWrites.push_back(
+			vk::WriteDescriptorSet(
+				m_DescriptorSets[inInfo.frameIndex],
+				1,
+				0,
+				1, vk::DescriptorType::eAccelerationStructureKHR,
+				nullptr,
+				nullptr,
+				nullptr,
+				&accelerationStructureInfo
+			)
+		);
+
+		// Push all the update to the descriptor set (so it's actually updated)
+		VulkanContext::GetDevice().updateDescriptorSets(
+			descriptorWrites.size(), descriptorWrites.data(),
+			0,
+			nullptr
+		);
+
+		// Bind the descriptor set (allow shader to access the data stored in the descriptor set)
+		inInfo.commandBuffer.bindDescriptorSets(
+			vk::PipelineBindPoint::eRayTracingKHR,
+			m_Pipeline.GetLayout(),
+			0,
+			1, &m_DescriptorSets[inInfo.frameIndex],
+			0,
+			nullptr
+		);
+	}
+
+	// Trace rays
+	auto rgen = m_Pipeline.GetRaygenSbt();
+	auto rmiss = m_Pipeline.GetMissSbt();
+	auto rchit = m_Pipeline.GetHitSbt();
+	auto rcall = m_Pipeline.GetCallableSbt();
+	inInfo.commandBuffer.traceRaysKHR(
+		rgen, rmiss, rchit, rcall,
+		inInfo.width,
+		inInfo.height,
+		1,
+		VulkanContext::GetDispatcher()
+	);
 }
--- Engine/Source/Runtime/Renderer/Public/SceneRenderer.h
+++ Engine/Source/Runtime/Renderer/Public/SceneRenderer.h
@@ -1,20 +1,98 @@
-#pragma once
+﻿#pragma once
 
+#include "Renderer_API.h"
 #include "VulkanRayTracingPipeline.h"
+#include "VoxelAccelerationStructure.h"
 
 #include <vulkan/vulkan.hpp>
+#include <vector>
 
-class SceneRenderer
+class RENDERER_API SceneRenderer
 {
 public:
+	static SceneRenderer Create(uint32_t frameCount);
 
 protected:
-	SceneRenderer();
+	SceneRenderer(
+		VulkanRaytracingPipeline&& pipeline,
+		VoxelAccelerationStructure&& accelerationStructure,
+		vk::DescriptorPool&& descriptorPool,
+		std::vector<vk::DescriptorSetLayout>&& descriptorSetLayouts,
+		std::vector<vk::DescriptorSet>&& descriptorSets
+	)
+		: m_Pipeline(std::move(pipeline)),
+		m_AccelerationStructure(std::move(accelerationStructure)),
+		m_DescriptorPool(std::move(descriptorPool)),
+		m_DescriptorSetLayouts(std::move(descriptorSetLayouts)),
+		m_DescriptorSets(std::move(descriptorSets))
+	{}
+
+public:
+	SceneRenderer() = default;
 	~SceneRenderer();
 
-protected:
-	vk::DescriptorSetLayout m_DescriptorSetLayout;
-	vk::DescriptorSet m_DescriptorSet;
-	VulkanRayTracingPipeline m_Pipeline;
+	SceneRenderer(SceneRenderer&& other) noexcept
+		: m_Pipeline(std::move(other.m_Pipeline)),
+		m_AccelerationStructure(std::move(other.m_AccelerationStructure)),
+		m_DescriptorPool(std::move(other.m_DescriptorPool)),
+		m_DescriptorSetLayouts(std::move(other.m_DescriptorSetLayouts)),
+		m_DescriptorSets(std::move(other.m_DescriptorSets))
+	{
+		other.m_Pipeline = nullptr;
+		other.m_AccelerationStructure = nullptr;
+		other.m_DescriptorPool = nullptr;
+		other.m_DescriptorSetLayouts.clear();
+		other.m_DescriptorSets.clear();
+	}
+	SceneRenderer& operator=(SceneRenderer&& rhs) noexcept
+	{
+		if (this != &rhs)
+		{
+			m_Pipeline = std::move(rhs.m_Pipeline);
+			m_AccelerationStructure = std::move(rhs.m_AccelerationStructure);
+			m_DescriptorPool = std::move(rhs.m_DescriptorPool);
+			m_DescriptorSetLayouts = std::move(rhs.m_DescriptorSetLayouts);
+			m_DescriptorSets = std::move(rhs.m_DescriptorSets);
 
+			rhs.m_Pipeline = nullptr;
+			rhs.m_AccelerationStructure = nullptr;
+			rhs.m_DescriptorPool = nullptr;
+			rhs.m_DescriptorSetLayouts.clear();
+			rhs.m_DescriptorSets.clear();
+		}
+
+		return *this;
+	}
+
+public:
+	struct RenderSceneInfo
+	{
+		uint32_t frameIndex;
+		uint16_t width;
+		uint16_t height;
+		vk::ImageView outputImage;
+		vk::CommandBuffer commandBuffer;
+
+		RenderSceneInfo(
+			uint32_t inFrameIndex,
+			const uint16_t inWidth,
+			const uint16_t inHeight,
+			const vk::ImageView inOutputImage,
+			const vk::CommandBuffer inCommandBuffer
+		)
+			: frameIndex(inFrameIndex),
+			width(inWidth),
+			height(inHeight),
+			outputImage(inOutputImage),
+			commandBuffer(inCommandBuffer)
+		{}
+	};
+	void RenderScene(const RenderSceneInfo& inInfo);
+
+protected:
+	VulkanRaytracingPipeline m_Pipeline;
+	VoxelAccelerationStructure m_AccelerationStructure;
+	vk::DescriptorPool m_DescriptorPool;
+	std::vector<vk::DescriptorSetLayout> m_DescriptorSetLayouts;
+	std::vector<vk::DescriptorSet> m_DescriptorSets;
 };
