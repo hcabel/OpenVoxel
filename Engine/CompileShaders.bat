c74d4209b2cdfa01bcd5708d78e3acb0bd3dd6c0
hcabel <hcabel@student.42.fr>
2023-09-21 10:33:04 +1000

    Rename Vendor file to Binaries #27 In fact I think I had forgot to move the vendor folder when migrating to the Engine folder :D So this is more like adding a Binaries folder, which will contain binaries for different tool that's required like premake and glslc

--- Engine/CompileShaders.bat
+++ Engine/CompileShaders.bat
@@ -7,9 +7,9 @@ set "SHADER_TARGET_PATH=intermediate/Shaders"
 
 if not exist "%SHADER_TARGET_PATH%" mkdir "%SHADER_TARGET_PATH%"
 
-call vendor\bin\glslc.exe --target-env=vulkan1.3 %SHADER_PATH%/raytrace.rgen -o %SHADER_TARGET_PATH%/RayGen.spv
-call vendor\bin\glslc.exe --target-env=vulkan1.3 %SHADER_PATH%/raytrace.rmiss -o %SHADER_TARGET_PATH%/Miss.spv
-call vendor\bin\glslc.exe --target-env=vulkan1.3 %SHADER_PATH%/raytrace.rchit -o %SHADER_TARGET_PATH%/Hit.spv
-call vendor\bin\glslc.exe --target-env=vulkan1.3 %SHADER_PATH%/raytrace.rint -o %SHADER_TARGET_PATH%/Intersection.spv
+call ..\Binairies\glslc.exe -g --target-env=vulkan1.3 %SHADER_PATH%/raytrace.rgen -o %SHADER_TARGET_PATH%/RayGen.spv
+call ..\Binairies\glslc.exe -g --target-env=vulkan1.3 %SHADER_PATH%/raytrace.rmiss -o %SHADER_TARGET_PATH%/Miss.spv
+call ..\Binairies\glslc.exe -g --target-env=vulkan1.3 %SHADER_PATH%/raytrace.rchit -o %SHADER_TARGET_PATH%/Hit.spv
+call ..\Binairies\glslc.exe -g --target-env=vulkan1.3 %SHADER_PATH%/raytrace.rint -o %SHADER_TARGET_PATH%/Intersection.spv
 
 pause
--- Engine/GenerateSolution.bat
+++ Engine/GenerateSolution.bat
@@ -1,2 +1,2 @@
-call vendor\bin\premake5.exe vs2022
+call ..\Binairies\premake5.exe vs2022
 pause
